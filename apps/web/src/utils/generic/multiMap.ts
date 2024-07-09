4965541ec12fc494ef7dc4d1a540ad22273f57e2
aparx <undestroy1310@gmail.com>
2024-07-09 21:39:25 +0200

    chore: added list factory to multimap

--- apps/web/src/utils/generic/multiMap.ts
+++ apps/web/src/utils/generic/multiMap.ts
@@ -1,5 +1,22 @@
+type ArrayFactory<K, V> = (key: K) => V[];
+
 export class MultiMap<K, V> extends Map<K, V[]> {
+  public static DEFAULT_ALLOCATOR: ArrayFactory<any, any> = () => [];
+
   private _map = new Map<K, V[]>();
+  private _factory: (key: K) => V[] = MultiMap.DEFAULT_ALLOCATOR;
+
+  constructor();
+  constructor(initialSize: number);
+  constructor(factory: ArrayFactory<K, V>);
+  constructor(arg?: number | ArrayFactory<K, V>) {
+    super();
+    if (typeof arg === "function") {
+      this._factory = arg;
+    } else if (typeof arg === "number") {
+      this._factory = () => new Array(arg);
+    }
+  }
 
   clear() {
     this._map.clear();
@@ -30,12 +47,25 @@ export class MultiMap<K, V> extends Map<K, V[]> {
   }
 
   push(key: K, value: V) {
-    const array = this._map.get(key) ?? [];
+    const array = this._map.get(key) ?? this.createArray(key);
     this._map.set(key, [...array, value]);
+    return this;
   }
 
   retain(key: K, filterPredicate: (val: V) => boolean) {
-    const array = this._map.get(key) ?? [];
+    const array = this._map.get(key) ?? this.createArray(key);
     this._map.set(key, array.filter(filterPredicate));
+    return this;
+  }
+
+  remove(key: K, removePredicate: (val: V) => boolean) {
+    return this.retain(key, (val) => !removePredicate(val));
+  }
+
+  createArray(key: K) {
+    const newArray = this._factory(key);
+    // Additional assertion at compile time, even if not needed
+    if (newArray == null) throw new Error("Array cannot be null");
+    return newArray;
   }
 }
